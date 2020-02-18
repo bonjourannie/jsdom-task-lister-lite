55643d718a08aa981bce458c7fb384f186059c7b
bonjourannie <artruex@gmail.com>
2020-02-17 17:20:30 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  addTaskToDoWithDeleteOption();
 });
+
+function addTaskToDoWithDeleteOption() {
+  const form = document.getElementById("create-task-form");
+  
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const userInput = event.target.querySelector("#new-task-description");
+    const showLists = document.getElementById("tasks");
+    const li = document.createElement("li");
+    showLists.appendChild(li);
+    li.textContent = userInput.value; 
+
+    const deleteButton = document.createElement("button");
+    li.appendChild(deleteButton);
+    deleteButton.textContent = "x";
+
+    userInput.value = ""; 
+    
+    deleteButton.addEventListener("click", () => {
+      const result = confirm("Want to delete");
+      if (result) {
+        li.remove();
+      }
+    })
+  })
+}
